c01ed7b55c941b8b90f7fb0c3dcd98b82d5f74df
Dennis <dennisgurban44@gmail.com>
2025-09-18 19:33:37 +0400

    Sliding window example added.

--- test.py
+++ test.py
@@ -1,17 +1,3 @@
-def find_max_sub_array(arr: list[int]) -> int:
-  sum_of_max_subarray = arr[0]
-
-  sum_of_curr_subarray = 0
-
-  for num in arr:
-    if sum_of_curr_subarray < 0:
-      sum_of_curr_subarray = 0
-
-    sum_of_curr_subarray += num
-
-    sum_of_max_subarray = max(sum_of_curr_subarray, sum_of_max_subarray)
-
-  return sum_of_max_subarray
-
-print('find_max_sub_array([-2, 1, -3, 4, -1, 2, 1, -5, 4]):',
-      find_max_sub_array([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+# Python's built-in print() function will accept an 
+# argument that is passed to it and print it to the
+# screen.
